332be89a7143d54d6ec00a21051a0d876e927d19
Ameer Badri <ameerbadri@gmail.com>
2021-03-18 12:20:36 +0000

    Update index.vue
    Modified season and sales discount

--- pages/index.vue
+++ pages/index.vue
@@ -6,9 +6,9 @@
       <template v-slot:img>
         <img src="/bag.jpg" alt="bag" />
       </template>
-      <template v-slot:new>50%</template>
+      <template v-slot:new>70%</template>
       <template v-slot:sale>Storewide Sale</template>
-      <template v-slot:collection>Summer</template>
+      <template v-slot:collection>Spring</template>
       <template v-slot:details>All accessories</template>
     </app-textlockup>
     <app-featured-products />
